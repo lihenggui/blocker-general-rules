3581a1c76a036bd00f2de6099e83eb0b6b1a12a2
lihenggui <lihenggui@users.noreply.github.com>
2022-12-30 17:07:05 -0800

    Remove CSVGenerator

--- components/CSVGenerator/src/main/kotlin/data/InstantComponentInfo.kt
+++
@@ -1,8 +0,0 @@
-package data
-
-data class InstantComponentInfo(
-    val packagePath: String,
-    val componentName: String,
-    val description: String,
-    val recommendToBlock: Boolean
-)
--- components/CSVGenerator/src/main/kotlin/data/Set.kt
+++
@@ -1,7 +0,0 @@
-package data
-
-data class Set(
-    var filename: String = "",
-    var date: Long = 0L,
-    var version: Long = 0L
-)
